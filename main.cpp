bf2ce25952431e1fa854ce0d08837005d4c7d6a8
arunsakthianand <55075079+arunsakthianand@users.noreply.github.com>
2021-12-21 09:43:04 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    cin>>i;
+    while(i--)
+    {
+        int j;
+        cin>>j;
+        int x[j],y[j];
+        while(j--)
+        {
+            cin>>x[j]>>y[j];
+        }
+        
+    }
+    return 0;
+}
